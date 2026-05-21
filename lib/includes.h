6941e03d10ee6609e84de496d70a916b5041a4de
benjaminhm <benjamin.hern.m@gmail.com>
2026-05-20 23:29:22 -0400

    Rfid funcional

---
+++ lib/includes.h
@@ -0,0 +1,5 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 5 // SDA del rc522 conectado al pin d5 en la esp32
+#define RST_PIN 21 // RST del rc522 conectado al pin d21 en la esp32
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include "../lib/includes.h"
 
-// put function declarations here:
-int myFunction(int, int);
+MFRC522 rfid(SS_PIN, RST_PIN); // Create an instance of the MFRC522 class
+unsigned long lastRead = 0;
+const unsigned long RESET_INTERVAL = 10000;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); // Initialize serial communication
+  Serial.println("Sistema inicianod - Gestion de turnos");
+  SPI.begin(); // Initialize SPI bus
+  rfid.PCD_Init(); // Initialize the MFRC522 RFID reader
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  if (millis() - lastRead > RESET_INTERVAL) {
+    rfid.PCD_Init();
+    lastRead = millis();
+  }
+
+  if (!rfid.PICC_IsNewCardPresent()){
+
+    delay(50); 
+    return;
+  }
+
+
+  if (!rfid.PICC_ReadCardSerial()){
+    return;
+  }
+
+  Serial.print("UID tag :");
+  for (byte i = 0; i < rfid.uid.size; i++){
+    Serial.print(rfid.uid.uidByte[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+  lastRead = millis();
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
